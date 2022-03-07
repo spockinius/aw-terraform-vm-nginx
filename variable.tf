4bd37282ad107502ea71d42d80bf535dd07f911b
katariina <katariina.vakkuri@awacademy.training>
2022-03-07 15:45:04 +0200

    exc 4

---
+++ variable.tf
@@ -0,0 +1,9 @@
+#Azure authentication variables
+variable "subscription_id" { }
+variable "tenant_id" { }
+# Network
+variable "network-vnet-cidr" { }
+variable "vm-subnet-cidr" { }
+# nginx VM
+variable "nginx_vm_size" { }
+variable "nginx_admin_username" { }
